9d4fce4021b735aad756e72dcbebee92de5de3a8
kimchheng-dev <k.kimchheng@gmail.com>
2020-07-13 15:06:44 +0700

    Newest commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def main(msg):
+    //Update another comment
     //Adding the second comment to this file.
-    //Print a message
+    //Print
     print(msg)
 
 print("Hello world")
